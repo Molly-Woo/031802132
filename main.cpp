bc8c766c9289d6a66ccf279b055c42a1f1b8f1d7
pollia <3152668032@qq.com>
2020-09-14 17:18:47 +0800

    create word embedding

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <windows.h>
+#include <time.h>
 #include "function/readin.cpp" 
 #include "cppjieba/include/cppjieba/Jieba.hpp"
+const int MAXN=1e5;
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
 	string text1;
 	string text2;
-	 if(argc!=3)
+	string dic_words[MAXN];
+	int dic_words_adr=0;
+	int len_text1=0,len_text2=0;
+	int vector_text1[MAXN]={0};
+	int vector_text2[MAXN]={0};
+	if(argc!=3)
     {
         printf("没有两个参数噢");
         return -1;
@@ -30,31 +37,62 @@ int main(int argc, char const *argv[])
         		IDF_PATH,
         		STOP_WORD_PATH);
         		
-    vector<string> words;
+    vector<string> words_text1;
+    vector<string> words_text2;
     vector<cppjieba::Word> jiebawords;
     string s;
     string result;
     
-	jieba.Cut(text1, words, true);
-	for (int i = 0;i < words.size();i++)
+	jieba.Cut(text1, words_text1, true);
+	len_text1=words_text1.size(); 
+	for (int i = 0;i < len_text1 ;i++)
 	{
-	cout << Utf8ToGbk(words[i].c_str()) << "/";
+		int flag=1;
+		for(int j = 0;j < dic_words_adr;j++){
+			if(dic_words[j]==words_text1[i]){
+				flag=0;break;
+			}
+		}
+		if(flag){
+			dic_words[dic_words_adr++]=words_text1[i];//需增加容错选项 
+		}
 	}
 	
-	cout<<endl; 
-	cout<<endl;
-	cout<<endl;
-	cout<<endl;
-	cout<<endl;
-	cout<<endl;
-	cout<<endl;
-	
-	jieba.Cut(text2, words, true);
-	for (int i = 0;i < words.size();i++)
+	jieba.Cut(text2, words_text2, true);
+	len_text2 = words_text2.size();
+	for (int i = 0;i < len_text2;i++)
 	{
-	cout << Utf8ToGbk(words[i].c_str()) << "/";
+		int flag=1;
+		for(int j = 0;j < dic_words_adr;j++){
+			if(dic_words[j]==words_text2[i]){
+				flag=0;break;
+			}
+		}
+		if(flag){
+			dic_words[dic_words_adr++]=words_text2[i];
+		}
 	}
-	cout << endl;
-	
+	for (int i=0;i < len_text1;i++){
+		for (int j=0;j < dic_words_adr;j++){
+			if(dic_words[j]==words_text1[i]){
+				vector_text1[j]++;break;				
+			}
+		}
+	}
+	for (int i=0;i < len_text2;i++){
+		for (int j=0;j < dic_words_adr;j++){
+			if(dic_words[j]==words_text2[i]){
+				vector_text2[j]++;break;				
+			}
+
+		}
+	}
+	/*
+	for (int i=0;i < dic_words_adr;i++){
+		cout << Utf8ToGbk(dic_words[i].c_str()) << "/";
+	}*/
+	cout <<(double)clock() /CLOCKS_PER_SEC<< "s" << endl;
     return 0;
 }
+
+//main.exe D:\A_valuable\procedure\c\1.txt D:\A_valuable\procedure\c\2.txt
